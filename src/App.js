79d88da1ab60e9039c3b6c82b3f5cec588d3dd1e
Ian Brown <ian@iabdesigns.com>
2022-03-17 11:51:28 -0700

    Initial commit for creating the development for this Age Game portfolio piece.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Initial development for this Age Game
+    </div>
+  );
+}
+
+export default App;
